ce58e3acfc0d55e1badc9189f5f85ced7fd09b9d
Olivia-Debora Moisi <deboramoisi@yahoo.com>
2023-01-08 23:16:08 +0200

    third commit

--- src/app/teaching/teaching.component.ts
+++ src/app/teaching/teaching.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-teaching',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teaching.component.css']
 })
 export class TeachingComponent implements OnInit {
+  checkIcon = faCheck;
+  course: string = ''; 
 
   constructor() { }
 
   ngOnInit(): void {
+    this.course = 'default';
+  }
+
+  scroll(el: HTMLElement, course: string) {
+    this.course = course;
+    el.scrollIntoView();
   }
 
 }
